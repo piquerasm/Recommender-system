c7bfbb85f0219bdd01d624fb3a8003a729722d9e
Jason Pruitt <pruittj1@wit.edu>
2021-03-23 22:43:30 -0400

    Update Main.java

--- Recommender/src/Main.java
+++ Recommender/src/Main.java
@@ -1,4 +1,12 @@
 
 public class Main {
+Scanner input = new Scanner(System.in);
+		//Jason's code. You can change whatever you see
+		String username;
+		System.out.println("Hello! Welcome to [PLACEHOLDERNAME]!");
+		System.out.print("Please type in your username: ");
+		username = input.next();
+		System.out.printf("Hello %d !",username);
+	}
 
 }
